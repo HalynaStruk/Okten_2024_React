19315eea8c03df7513384e4c41790973af29f877
HalynaStruk <halynkastruk@gmail.com>
2024-05-01 18:16:29 +0300

    lesson 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
+import Character from "./components/character/Character";
 
 const App = () => {
 
 
   return (
       <div>
-
-
+          <Character/>
       </div>
   );
 };
