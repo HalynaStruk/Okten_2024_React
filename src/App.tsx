fe4d180528281b848a92677bfe7157b242ca2928
HalynaStruk <halynkastruk@gmail.com>
2024-05-19 16:36:33 +0300

    lesson 2 axios, state lifting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import PostsComponent from "./components/posts/PostsComponent";
+import CommentsComponent from "./components/comments/CommentsComponent";
+import {getAllCommentsOfSinglePost} from "./services/jpg.api.service";
+import {ICommentModel} from "./models/ICommentModel";
 
 const App = () => {
 
+    const [comments, setComments] = useState<ICommentModel[]>([]);
+
+    const lift = (postId:number) => {
+        getAllCommentsOfSinglePost(postId).then(({data}) => {
+            setComments(data);
+        })
+    }
 
   return (
-      <div>
-
-
+      <div  className="App">
+          <div className={'posts'}><PostsComponent lift={lift}/></div>
+          <div className={'comments'}>
+              <CommentsComponent comments={comments}/>
+          </div>
       </div>
   );
 };
