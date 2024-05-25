4ef2e9cc43d0d65d97a6943249803b6c132a56cf
HalynaStruk <halynkastruk@gmail.com>
2024-05-25 18:29:27 +0300

    homework

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,16 +1,15 @@
-import React from "react";
-import useToggle from "../hooks/useToggle";
-
-const ToggleComponent: React.FC = () => {
-    // Використовуємо хук useToggle
-    const [isToggled, toggle] = useToggle();
-
-    return (
-        <div>
-            <p>The current value is {isToggled.toString()}</p>
-            <button onClick={toggle}>Toggle</button>
-        </div>
-    );
-}
-
-export default ToggleComponent;
+// import React from "react";
+//
+// const ToggleComponent: React.FC = () => {
+//     // Використовуємо хук useToggle
+//     const [isToggled, toggle] = useToggle();
+//
+//     return (
+//         <div>
+//             <p>The current value is {isToggled.toString()}</p>
+//             <button onClick={toggle}>Toggle</button>
+//         </div>
+//     );
+// }
+//
+// export default ToggleComponent;
--- src/hooks/useToggle.tsx
+++ src/hooks/useToggle.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { useState, useCallback } from "react";
-
-// Типи для хуку useToggle
-type UseToggle = [boolean, () => void];
-
-// Створення функції useToggle
-function useToggle(initialValue: boolean = false): UseToggle {
-    // Ініціалізуємо стан з початковим значенням
-    const [value, setValue] = useState<boolean>(initialValue);
-
-    // Використовуємо useCallback для запам'ятовування функції toggle
-    function toggle(value) {
-        setValue((prevValue) => !prevValue);
-    }
-    // Повертаємо поточне значення і функцію перемикання
-    return [value, toggle];
-}
-
-export default useToggle;
-
+// import React from "react";
+// import { useState, useCallback } from "react";
+//
+// // Типи для хуку useToggle
+// type UseToggle = [boolean, () => void];
+//
+// // Створення функції useToggle
+// function useToggle(initialValue: boolean = false): UseToggle {
+//     // Ініціалізуємо стан з початковим значенням
+//     const [value, setValue] = useState<boolean>(initialValue);
+//
+//     // Використовуємо useCallback для запам'ятовування функції toggle
+//     function toggle(value) {
+//         setValue((prevValue) => !prevValue);
+//     }
+//     // Повертаємо поточне значення і функцію перемикання
+//     return [value, toggle];
+// }
+//
+// export default useToggle;
+//
