4a7e99d58b37c2d33485afe2a1657a6450b50b66
HalynaStruk <halynkastruk@gmail.com>
2024-05-29 18:23:47 +0300

    lesson 5 useForm, validation, joi-validation

--- src/components/FormComponentValidationJOI.tsx
+++ src/components/FormComponentValidationJOI.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import {useForm} from "react-hook-form";
+
+interface IFormProps {
+    username: string,
+    age: number
+    password: string,
+}
 
 const FormComponentValidationJOI = () => {
+    let {register, handleSubmit} = useForm<IFormProps>();
+
+    const save = (formValues: IFormProps) => {
+        console.log(formValues);
+    };
+
     return (
         <div>
-
+            <form onSubmit={handleSubmit(save)}>
+                <input type="text" {...register('username')}/>
+                <input type="number" {...register('age')}/>
+                <input type="text" {...register('password')}/>
+                <button>save</button>
+            </form>
         </div>
     );
 };
