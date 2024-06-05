b68ed784af2ea8ebd497daefeda4aeb08ec80519
HalynaStruk <halynkastruk@gmail.com>
2024-06-05 23:28:15 +0300

    homework 7 router useParams, useLocation, state

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,9 +2,11 @@ import {IRes} from "../types/resType";
 import {IUser} from "../interfaces/userInterface";
 import {apiService} from "./apiService";
 import {urls} from "../urls/urls";
+import {IPost} from "../interfaces/postInterface";
 
 const userService = {
-    getAll: (): IRes<IUser[]> => apiService.get(urls.users)
+    getAll: (): IRes<IUser[]> => apiService.get(urls.users),
+    getPostsByUserId: (userId:number): IRes<IPost[]> => apiService.get(urls.users+'/'+userId+urls.posts)
 }
 
 export {
